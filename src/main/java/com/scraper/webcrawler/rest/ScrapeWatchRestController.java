44393802ed1a6b8592e71b638c47ecf9755fef63
Nikola Atanasov <n.atanasov1994@google.com>
2023-08-20 16:24:50 +0200

    Remove unused imports

--- src/main/java/com/scraper/webcrawler/rest/ScrapeWatchRestController.java
+++ src/main/java/com/scraper/webcrawler/rest/ScrapeWatchRestController.java
@@ -1,12 +1,6 @@
 package com.scraper.webcrawler.rest;
 
 import java.io.IOException;
-import java.util.List;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
