8a84e3798e7eb7221d37e6b1f465492b537e0f37
Nikola Atanasov <n.atanasov1994@google.com>
2023-08-21 00:09:25 +0200

    Remove banner; Remove unused import

--- src/test/java/com/scrapewatch/scrape/ScraperTest.java
+++ src/test/java/com/scrapewatch/scrape/ScraperTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 
 import com.scrapewatch.dto.ScrapedPageDTO;
 import com.scrapewatch.jsoup.JsoupConnection;
